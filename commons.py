d7ccd1d48e505b1559154a22dbdaf6e0c10d804c
Kiritee <kiritee.amruth@innovaccer.com>
2017-06-21 19:44:20 +0530

    modified particle gossip method

--- commons.py
+++ commons.py
@@ -1,2 +1,8 @@
 def distance(point1, point2):
 	return round(reduce(lambda a,b : a+b ,list(map(lambda x : x**2, list(point1-point2)))),3)
+
+def plotPoints(resource_locations, particle_locations):
+	plt.scatter(zip(*resource_locations)[0],zip(*resource_locations)[1], c = 'r')
+	plt.scatter(zip(*particle_locations)[0],zip(*particle_locations)[1], c = 'b')
+	plt.xlim(0,1); plt.ylim(0,1)
+	plt.show()
--- particleTraversalGrids.py
+++ particleTraversalGrids.py
@@ -8,19 +8,24 @@ global maxGridDistance
 maxGridDistance = np.sqrt(2)
 
 def generateResourceCoordinates(number_of_resources):
-	return np.array([[float("%.2f" %rd.uniform(0,1)),float("%.2f" %rd.uniform(0,1))] for e in range(number_of_resources)])
+	return np.array([[float("%.2f" %rd.uniform(0,1)),float("%.2f" %rd.uniform(0,1))] \
+		for e in range(number_of_resources)])
 
 def generateCoordinates(number_of_particles = 1, factor = 1):
-	return np.array([[float("%.2f" %rd.uniform(0,1))*factor,float("%.2f" %rd.uniform(0,1))*factor] for e in range(number_of_particles)])
+	return np.array([[float("%.2f" %rd.uniform(0,1))*factor,float("%.2f" %rd.uniform(0,1))*factor]\
+	 for e in range(number_of_particles)])
 
 def generateVelocityVectors(number_of_particles = 1, factor = 0.5):
-	return np.array([[float("%.2f" %rd.uniform(-0.5, 0.5))*factor,float("%.2f" %rd.uniform(-0.5, 0.5))*factor] for e in range(number_of_particles)])
+	return np.array([[float("%.2f" %rd.uniform(-0.5, 0.5))*factor,float("%.2f" %rd.uniform(-0.5, 0.5))*factor]\
+	 for e in range(number_of_particles)])
 
 def getNextTimestepCoordinates(coordinate_vectors, velocity_vectors):
-	return _tackleOverTheGridCoords([_createMutation(list(np.array(c)+np.array(v))) for c,v in zip(coordinate_vectors, velocity_vectors)])
+	return _tackleOverTheGridCoords([_createMutation(list(np.array(c)+np.array(v)))\
+	 for c,v in zip(coordinate_vectors, velocity_vectors)])
 
 def remakeVelocityVectors(resource_locations_found, velocity_vectors):
-	return [np.array([0,0]) if i in resource_locations_found.keys() else vel for i, vel in enumerate(velocity_vectors)]
+	return [np.array([0,0]) if i in resource_locations_found.keys() else vel\
+	 for i, vel in enumerate(velocity_vectors)]
 
 def _createMutation(list_of_coordinates):
 	if np.random.choice([True, False], p = [0.01,0.99]):
@@ -29,9 +34,19 @@ def _createMutation(list_of_coordinates):
 		return list_of_coordinates
 
 def _tackleOverTheGridCoords(list_of_coords):
-	return [[rd.uniform(0,1), rd.uniform(0,1)] if (not (0 < x < 1) or not(0 < y < 1)) else [x,y] for x,y in list_of_coords]
+	return [[rd.uniform(0,1), rd.uniform(0,1)] if (not (0 < x < 1) or not(0 < y < 1)) else [x,y]\
+	 for x,y in list_of_coords]
 
-def searchForResourceAround(current_locations, resource_locations, iteration, radius = 1):
+def searchOrGetMessage(settlers, current_locations, resource_locations, iteration, particle_locations, radius = 0.2):
+	(allresources_found_around, resources_found_by_distance_alone) = searchForResourceAround(current_locations,\
+	 resource_locations, iteration, radius)
+	print "all_resources_found : ", allresources_found_around
+	resources_found_by_message_received = getResourcesByGossip(settlers, 0.4,\
+	 allresources_found_around, resource_locations, particle_locations)
+	print "resources_found_by_message_received", resources_found_by_message_received
+	exit()
+
+def searchForResourceAround(current_locations, resource_locations, iteration, radius = 0.2):
 	found_locations_by_distance = {}
 	for i, particle in enumerate(current_locations):
 		for j, resource in enumerate(resource_locations):
@@ -41,13 +56,15 @@ def searchForResourceAround(current_locations, resource_locations, iteration, ra
 					found_locations_by_distance[i].append([j, distance(particle, resource)])
 					continue
 				found_locations_by_distance[i].append([j, distance(particle, resource)])
-	return getBestResources(iteration, found_locations_by_distance)
+	return found_locations_by_distance, getBestResources(iteration, found_locations_by_distance)
 
 def getBestResources(iteration, found_locations_by_distance, signals_available = None):
-	return { particle : getBestResourceForAParticle(iteration, resources) for particle, resources in found_locations_by_distance.iteritems()}
+	return { particle : getBestResourceForAParticle(iteration, resources)\
+	 for particle, resources in found_locations_by_distance.iteritems()}
 
 def getBestResourceForAParticle(iteration, location_of_resources):
-	scores = { l : getScoreBySignals(iteration, dist) + getScoreByNumberOfIterationsSpent(iteration) for i, (l,dist) in enumerate(location_of_resources)}
+	scores = { l : getScoreBySignals(iteration, dist) + getScoreByNumberOfIterationsSpent(iteration)\
+	 for i, (l,dist) in enumerate(location_of_resources)}
 	min_score = min(scores.values())
 	return scores.keys()[scores.values().index(min_score)]
 
@@ -56,35 +73,37 @@ def getScoreByNumberOfIterationsSpent(iteration):
 
 def getScoreBySignals(iteration, dist):
 	return round(np.exp((maxGridDistance - dist)/maxGridDistance), 2)
-
-def plotPoints(resource_locations, particle_locations):
-	plt.scatter(zip(*resource_locations)[0],zip(*resource_locations)[1], c = 'r')
-	plt.scatter(zip(*particle_locations)[0],zip(*particle_locations)[1], c = 'b')
-	plt.xlim(0,1); plt.ylim(0,1)
-	plt.show()
 	
-def gossip(max_gossip_distance, settlers, resource_locations, particle_locations):
+def getResourcesByGossip(settlers, max_gossip_distance, settlers, resource_locations, particle_locations):
 	'''
-		
+		Args:
+			max_gossip_distance (float) : A particle sends a gossip around a radius of max_gossip_distance
+			settlers (dict) : Map with resource number and list of particles that are settled for this
+			resource_locations : Map with resource number and the location of it on map
+			particle_locations : Map with particle index and the current location of particles
+
+		Returns:
+			Note : SOME FUNCTIONS NEEDS TO BE ADDED
 	'''
 	p_received_gossip = {}
 	mod_settlers_by_resource = {}
 	'''
-		gives {resource_n : [particle_m,...,particle_n]}
+		Gives {r_m : [ [p_n, d_n-r_m ] ]}
 	'''
 	for k,v in settlers.iteritems():
-		if v not in mod_settlers_by_resource:
-			mod_settlers_by_resource[v] = []
-			mod_settlers_by_resource[v].append(k)
-			continue
-		mod_settlers_by_resource[v].append(k)
+		for i, [res_index, distance] in enumerate(v):
+			if res_index not in mod_settlers_by_resource:
+				mod_settlers_by_resource[res_index] = []
+				mod_settlers_by_resource[res_index].append([k,distance])
+			else:
+				mod_settlers_by_resource[res_index].append([k,distance])
 	'''
 		Should output a map of resources and msg received particles
 	'''
 	ret = {}
-	for i, (resource_index, particles_allocated) in mod_settlers_by_resource.iteritems():
-		for j, (particle_index_dummy, current_location) in particle_locations.iteritems():
-			if (particle_index not in particles_allocated) and \
+	for i, (resource_index, particles_received) in mod_settlers_by_resource.iteritems():
+		for j, (particle_index, distance) in enumerate(particle_locations):
+			if (particle_index not in settlers[resource_index]) and \
 				distance(current_location, resource_locations[resource_allocated_to]):
 					if particle_index_dummy not in ret:
 						ret[particle_index_dummy] = []
@@ -96,12 +115,13 @@ def main(number_of_resources = 20, number_of_particles = 2):
 	resource_vectors = generateResourceCoordinates(number_of_resources)
 	coordinate_vectors = generateCoordinates(number_of_particles)
 	velocity_vectors = generateVelocityVectors(number_of_particles)
+	found_resource_locations = {}
+	settlers = {}
 	while iterations <= 10:
 		# plotPoints(resource_vectors, coordinate_vectors)
-		found_resource_locations = searchForResourceAround(coordinate_vectors, resource_vectors, iterations)
+		found_resource_locations = searchOrGetMessage(coordinate_vectors, resource_vectors, iterations, found_resource_locations)
 		velocity_vectors = remakeVelocityVectors(found_resource_locations, velocity_vectors)
 		coordinate_vectors = getNextTimestepCoordinates(coordinate_vectors,velocity_vectors)
-		settlers_locations = None
 		iterations += 1
 
 if __name__ == '__main__':
