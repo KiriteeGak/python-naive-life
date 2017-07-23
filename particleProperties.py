f28db584acdb61ac91b17983cc284bf8a49c0c20
KiriteeGak <kiritee.gandamalla@gmail.com>
2017-07-24 04:45:25 +0530

    Incorporated addn. particle props

--- particleProperties.py
+++ particleProperties.py
@@ -1,11 +1,10 @@
-import random as rd
-
-
 class ParticleProps(object):
     """
         :argument
             position (List) : Gives the position of the particle at the current iteration
             velocity (List) : Gives velocity vector
+            prev_positions (List) : Previous positions the particle has gone threw
+            prev_velocities (List) : Previous velocities the particle has possessed in resp. particle position
             settled (Boolean) : Shows whether a particle has settled to a resource. Found a resource does not mean settled
             moving_to_resource : Shows whether the particle has currently and is headed in that direction
             searching : Particle still in search mode
@@ -13,9 +12,11 @@ class ParticleProps(object):
             ParticleProps class instance
     """
 
-    def __init__(self, position, velocity, settled, moving_to_resource, searching):
+    def __init__(self, position, velocity, prev_positions, prev_velocities, settled, moving_to_resource, searching):
         self.position = position
         self.velocity = velocity
+        self.prev_positions = prev_positions
+        self.prev_velocities = prev_velocities
         self.settled = settled
         self.moving_to_resource = moving_to_resource
         self.searching = searching
